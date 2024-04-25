e64cb32efa1ae5c4c1cb13b7b9df164acc3c04e4
Yahor Harunovich <pixel.egor@gmail.com>
2024-04-25 12:22:19 +0200

    task01

---
+++ task01/src/lambdas/hello_world/handler.py
@@ -0,0 +1,24 @@
+from commons.log_helper import get_logger
+from commons.abstract_lambda import AbstractLambda
+
+_LOG = get_logger('HelloWorld-handler')
+
+
+class HelloWorld(AbstractLambda):
+
+    def validate_request(self, event) -> dict:
+        pass
+        
+    def handle_request(self, event, context):
+        return {
+            "statusCode": 200,
+            "message": "Hello from Lambda"
+
+        }
+    
+
+HANDLER = HelloWorld()
+
+
+def lambda_handler(event, context):
+    return HANDLER.lambda_handler(event=event, context=context)
